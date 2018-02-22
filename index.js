7bf6aa5b882fa98f48752af12aeae44ca2ea876d
Brittany Barrow <bangelabarrow@gmail.com>
2018-02-22 18:59:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
+var name = "Susan"
 if (name === "Susan") {
   console.log("The name is correct")
-} else {
-  console.log("Expected the name to be Susan")
-}
-var name = "Susan"
-var height = "74"
+} 
 
 
 // Don't worry about this
